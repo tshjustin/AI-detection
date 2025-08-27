79b3c4a28af56586268742d3b2cf29badf26ecf5
tshjustin <j.tann2305@gmail.com>
2025-08-27 23:54:23 +0800

    added ping endpoint

--- main.py
+++ main.py
@@ -1,23 +1,49 @@
-from fastapi.responses import JSONResponse
 from fastapi import FastAPI, UploadFile, File
-
-import shutil
-import tempfile
+from fastapi.responses import JSONResponse
 
 from transformers import pipeline
 
-detector = pipeline("video-classification", model="tayyabimam/Deepfake")
+import tempfile
+import shutil
+import os
 
 app = FastAPI(title="Deepfake Detector API")
 
+detector = None
+
+@app.on_event("startup")
+async def load_model():
+    global detector
+    detector = pipeline("video-classification", model="tayyabimam/Deepfake")
+    print("Model loaded ")
+
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
+    """
     
-    # HF endpoint requires file handler 
+    Args:   
+        file: MP4 video 
+
+    Returns:
+        Json of the payload
+        {
+            "prediction": "REAL/FAKE", 
+            "confidence": float,
+            "prediction_code": int
+        }
+    """
+
+    # HF needs a file handler 
     with tempfile.NamedTemporaryFile(delete=False, suffix=".mp4") as tmp:
         shutil.copyfileobj(file.file, tmp)
         tmp_path = tmp.name
 
-    results = detector(tmp_path)
-    
-    return results
+    try:
+        results = detector(tmp_path)
+        return results
+    finally:
+        os.unlink(tmp_path) 
+
+@app.post('/ping')
+def ping():
+    return {"status": "ok"}
